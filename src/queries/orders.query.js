62c4518e3fa11e51ed5a3555688592c0e65a78e3
seintun <seintun.edu@gmail.com>
2018-10-11 19:40:58 -0700

    bugs/ticket-34-MVCQ-orders-restaurants

--- src/queries/orders.query.js
+++ src/queries/orders.query.js
@@ -2,10 +2,14 @@ const knex = require('./db')
 
 //Query returns a list of orders details
 const fetchOrders = () => {
-    return knex('orders')
-        .orderBy('created_at', 'DESC');
+    return knex('orders_items')
+        .join('orders', 'orders.id', '=', 'orders_items.order_id')
+        .join('items', 'items.id', '=', 'orders_items.item_id')
+        .join('restaurants', 'restaurants.id', '=', 'orders.restaurant_id')
+        .join('users', 'users.id', '=', 'orders.user_id')
+        .select('users.name as userName','orders.id as orderId','restaurants.name as restaurantName','orders.pretax as orderPretax', 'orders.tax as orderTax','orders.tips as orderTips')
 }
-//Query return a specified User with name & email
+//Query return a specified order with details
 const findOrder = (id) => {
     return knex('orders')
         .select(['orders.pretax'])

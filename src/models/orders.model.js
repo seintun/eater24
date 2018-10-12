5e9ab955e6b278027215f15b6daf982bf244df24
seintun <seintun.edu@gmail.com>
2018-10-11 23:51:15 -0700

    refactor/ticket-36-add-comment-orders-route

--- src/models/orders.model.js
+++ src/models/orders.model.js
@@ -1,5 +1,5 @@
 const ordersQuery = require('../queries/orders.query')
-
+//calling Query to fetch list of orders of specified restaurant
 const fetchOrders = (restaurantId) => {
   let orders = ordersQuery.fetchOrders(restaurantId)
 
@@ -9,6 +9,7 @@ const fetchOrders = (restaurantId) => {
       : result
   })
 }
+//calling Query to find specific order of specified restaurant
 const findOrder = (orderId, restaurantId) => {
   let order = ordersQuery.findOrder(orderId, restaurantId)
 
@@ -18,8 +19,18 @@ const findOrder = (orderId, restaurantId) => {
       : result
   })
 }
+//calling Query to create order with args pass
+const createOrder = (body, restaurantId) => {
+  let order = ordersQuery.createOrder(body, restaurantId)
 
+  return order.then(result => {
+    return !result
+      ? { error: 'error creating new order', status: 404 }
+      : result
+  })
+}
 module.exports = {
   fetchOrders,
-  findOrder
+  findOrder,
+  createOrder
 }
--- src/queries/orders.query.js
+++ src/queries/orders.query.js
@@ -2,28 +2,66 @@ const knex = require('./db')
 
 //Query returns a list of orders details
 const fetchOrders = (restaurantId) => {
-
+    // return innerJoin results from orders, items, restaurants, users tables
     return knex('orders_items')
         .join('orders', 'orders.id', '=', 'orders_items.order_id')
         .join('items', 'items.id', '=', 'orders_items.item_id')
         .join('restaurants', 'restaurants.id', '=', 'orders.restaurant_id')
         .join('users', 'users.id', '=', 'orders.user_id')
+        // Select certain columns and generate order report
         .select('users.name as userName','orders.id as orderId','restaurants.name as restaurantName','orders.pretax as orderPretax', 'orders.tax as orderTax','orders.tips as orderTips')
         .where('orders.restaurant_id', restaurantId)
+        // Remove any duplicate data
+        .distinct()
 }
 //Query return a specified order with details
 const findOrder = (orderId, restaurantId) => {
+    // return innerJoin results from orders, items, restaurants, users tables
     return knex('orders_items')
         .join('orders', 'orders.id', '=', 'orders_items.order_id')
         .join('items', 'items.id', '=', 'orders_items.item_id')
         .join('restaurants', 'restaurants.id', '=', 'orders.restaurant_id')
         .join('users', 'users.id', '=', 'orders.user_id')
+        // Select certain columns and generate order report
         .select('users.name as userName','orders.id as orderId','restaurants.name as restaurantName','orders.pretax as orderPretax', 'orders.tax as orderTax','orders.tips as orderTips')
         .where('orders.id', orderId)
         .where('orders.restaurant_id', restaurantId)
+        // Remove any duplicate data
+        .distinct()
     }
+//Query order table and assign each column with respective values from args: body and req.params.restaurantId for assigning its foreign key
+const createOrder = (body, restaurantId) => {
+    // console.log(body,'this is body inside QUERY')
+    // console.log(restaurantId,'this is resId inside QUERY')
+    return knex('orders')
+        .insert({
+            user_id:        body.user_Id,
+            restaurant_id:  restaurantId,
+            pretax:         "50",
+            tax:            "24",
+            tips:           "12"
+        })
+        .returning('id')
 
+        .then(result => {
+            let arrayOfObj = [];
+            //Return successful message once the entry is completed
+            body.items_id.map(item => {
+                let itemOrdered = {
+                                order_id:   `${result}`,
+                                item_id:    item
+                            }
+                    arrayOfObj.push(itemOrdered);
+                })
+            return knex('orders_items')
+                .insert(arrayOfObj);
+        })
+        .catch(err => {
+            return err.message;
+        })
+}
 module.exports = {
     fetchOrders,
-    findOrder
+    findOrder,
+    createOrder
 }

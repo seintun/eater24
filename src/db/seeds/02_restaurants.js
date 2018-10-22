870914dc20d3aa29302d5e41bcd0a2ee214c0d97
seintun <seintun.edu@gmail.com>
2018-10-21 22:42:54 -0700

    added additional restaurannts and items seeds

--- src/db/seeds/02_restaurants.js
+++ src/db/seeds/02_restaurants.js
@@ -27,6 +27,22 @@ exports.seed = function(knex, Promise) {
           phone: '4158650633',
           created_at: new Date(),
           updated_at: new Date()
+        },
+        {
+          name: 'Fog Harbor Fish House', 
+          address: 'Pier 39, Ste A-202, San Francisco, CA 94133', 
+          cuisine: 'Seafood Bar', 
+          phone: '4154212442',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          name: 'Marufuku Ramen SF', 
+          address: '1581 Webster St, Ste 235, San Francisco, CA 94115', 
+          cuisine: 'Japanese', 
+          phone: '4158729786',
+          created_at: new Date(),
+          updated_at: new Date()
         }
       ]);
     });
--- src/db/seeds/03_items.js
+++ src/db/seeds/03_items.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
           restaurant_id: 1, 
           name: 'Black Cod', 
           price: '25', 
-          descriptions: 'saskatchewan chanterelles, sunflower seed fat, spinach', 
+          descriptions: 'Saskatchewan chanterelles, sunflower seed fat, spinach', 
           allergens: 'sunflower seeds',
           created_at: new Date(),
           updated_at: new Date()
@@ -17,8 +17,26 @@ exports.seed = function(knex, Promise) {
           restaurant_id: 1, 
           name: 'Giant Clam', 
           price: '18', 
-          descriptions: 'buttermilk whey, smoked potato, celery', 
+          descriptions: 'Buttermilk whey, smoked potato, celery', 
           allergens: 'diary, seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 1, 
+          name: 'Creek Raised Trout', 
+          price: '27', 
+          descriptions: 'cured, smoked, warmed in cedar | sandwich of skin, roe, spine meat, horseradish mayonnaise', 
+          allergens: 'seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 1, 
+          name: 'Squash Blossom', 
+          price: '18', 
+          descriptions: 'Stuffed with lamb, grilled olives, pollen sauce blue maize cornbread & cultured butter', 
+          allergens: 'pollen',
           created_at: new Date(),
           updated_at: new Date()
         },
@@ -26,7 +44,7 @@ exports.seed = function(knex, Promise) {
           restaurant_id: 1, 
           name: 'Wild Boar Rib', 
           price: '32', 
-          descriptions: 'eucalyptus, fennel pollen, pine needle', 
+          descriptions: 'Eucalyptus, fennel pollen, pine needle', 
           allergens: 'pollen',
           created_at: new Date(),
           updated_at: new Date()
@@ -58,6 +76,95 @@ exports.seed = function(knex, Promise) {
           created_at: new Date(),
           updated_at: new Date()
         },
+        {
+          restaurant_id: 2, 
+          name: 'Seared Filet of Beef', 
+          price: '32', 
+          descriptions: 'Cumin Potatoes, Swiss Chard, Tomato-Corn Relish and Basil Butter', 
+          allergens: 'diary',
+          created_at: new Date(),
+          updated_at: new Date()
+        },        {
+          restaurant_id: 3, 
+          name: 'Hideaki Omakase', 
+          price: '150', 
+          descriptions: '2 Appetizer, 1 Sashimi, 10 pcs Nigiri, 1 Owan', 
+          allergens: 'seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 3, 
+          name: 'Yamato Omakase', 
+          price: '200', 
+          descriptions: '2 Appetizer, 2 Sashimi, 1 Yakimono, 12 pcs Nigiri, 1 Owan', 
+          allergens: 'seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 4, 
+          name: 'Ahi Tuna Poke', 
+          price: '18', 
+          descriptions: 'Marinated raw tuna, avocado, mango, sesame-soy sauce and wasabi aioli', 
+          allergens: 'seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 4, 
+          name: 'Shellfish Tower', 
+          price: '88', 
+          descriptions: '½ Maine lobster, ½ cracked Dungeness crab, 6 fresh shucked oysters, 6 jumbo shrimp, 6 mussels, Dungeness crab ceviche served with cocktail sauce and mignonette', 
+          allergens: 'seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 4, 
+          name: 'California Crab and Avocado Caesar Salad', 
+          price: '29', 
+          descriptions: 'Bacon, avocado, queso fresco, tortilla strips, cilantro leaves, chopped romaine topped with Dungeness crab meat', 
+          allergens: 'seafood',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 5, 
+          name: 'Hakata Tonkotsu', 
+          price: '13', 
+          descriptions: 'Chashu Pork (Simmered Pork Belly), Seasoned Soft Boiled Egg, Green Onions, Kikurage Mushrooms, and Bean Sprouts', 
+          allergens: 'mushroom ',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 5, 
+          name: 'Hakata Deluxe', 
+          price: '17', 
+          descriptions: 'Buta Kakuni (Braised Thick Pork Belly), Chashu Pork (Simmered Pork Belly), Seasoned Soft Boiled Egg, Green Onions, Kikurage Mushrooms, Bean Sprouts, Sweet Corn, and Nori Seaweed', 
+          allergens: 'mushroom',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 5, 
+          name: 'Chicken Paitan', 
+          price: '13', 
+          descriptions: 'Chashu Chicken (Chicken Breast), Seasoned Soft Boiled Egg, Green Onions, Kikurage Mushrooms, and Bean Sprouts', 
+          allergens: 'mushroom',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          restaurant_id: 5, 
+          name: 'Chicken Paitan Deluxe (Limited Servings)', 
+          price: '17', 
+          descriptions: 'Grilled Chicken Leg on a Sizzling Plate, Chashu Chicken (Chicken Breast), Seasoned Soft Boiled Egg, Green Onions, Kikurage Mushrooms, Bean Sprouts, Sweet Corn, and Nori Seaweed', 
+          allergens: 'mushroom',
+          created_at: new Date(),
+          updated_at: new Date()
+        }
       ]);
     });
 };

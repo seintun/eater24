2d2f2912cb2369c1f6cc6ed60b3ffa3f11604107
seintun <seintun.edu@gmail.com>
2018-10-23 17:25:29 -0700

    add additional data inside orders seed files

--- src/db/seeds/04_orders.js
+++ src/db/seeds/04_orders.js
@@ -15,6 +15,46 @@ exports.seed = function(knex, Promise) {
         {
           user_id: 3, 
           restaurant_id: 2
+        },
+        {
+          user_id: 2, 
+          restaurant_id: 3
+        },
+        {
+          user_id: 2, 
+          restaurant_id: 4
+        },
+        {
+          user_id: 1, 
+          restaurant_id: 5
+        },
+        {
+          user_id: 3, 
+          restaurant_id: 2
+        },
+        {
+          user_id: 1, 
+          restaurant_id: 3
+        },
+        {
+          user_id: 2, 
+          restaurant_id: 2
+        },
+        {
+          user_id: 1, 
+          restaurant_id: 3
+        },
+        {
+          user_id: 3, 
+          restaurant_id: 3
+        },
+        {
+          user_id: 1, 
+          restaurant_id: 2
+        },
+        {
+          user_id: 1, 
+          restaurant_id: 4
         }
       ]);
     });
--- src/db/seeds/05_orders_items.js
+++ src/db/seeds/05_orders_items.js
@@ -18,6 +18,56 @@ exports.seed = function(knex, Promise) {
           order_id: 3,
           item_id: 6,
           quantity: 3
+        },
+        {
+          order_id: 4,
+          item_id: 10,
+          quantity: 2
+        },
+        {
+          order_id: 5,
+          item_id: 14,
+          quantity: 3
+        },
+        {
+          order_id: 6,
+          item_id: 15,
+          quantity: 2
+        },
+        {
+          order_id: 7,
+          item_id: 6,
+          quantity: 3
+        },
+        {
+          order_id: 8,
+          item_id: 11,
+          quantity: 1
+        },
+        {
+          order_id: 9,
+          item_id: 9,
+          quantity: 2
+        },
+        {
+          order_id: 10,
+          item_id: 13,
+          quantity: 2
+        },
+        {
+          order_id: 11,
+          item_id: 12,
+          quantity: 2
+        },
+        {
+          order_id: 12,
+          item_id: 7,
+          quantity: 2
+        },
+        {
+          order_id: 13,
+          item_id: 12,
+          quantity: 2
         }
       ]);
     });
